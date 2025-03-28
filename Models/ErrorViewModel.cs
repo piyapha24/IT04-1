5e714a88d36336a7affefda74ccd63f5f3d6e5fe
Piyaphat <146691338+piyapha24@users.noreply.github.com>
2025-03-28 09:17:02 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT04_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
